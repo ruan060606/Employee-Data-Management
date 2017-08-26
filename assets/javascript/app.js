ad84aba67d17a3fe4bd55ef6807600dd85f76fdb
Stephen Ruan <sruan06@Stephens-MacBook-Pro.local>
2017-08-26 11:48:41 -0700

    css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,25 +5,37 @@ var $monthlyRateField = $("#monthly_rate_field");
 var $buttonSubmit = $("#button_submit");
 
 // Initialize Firebase
-function initFirebase() {
-  var config = {
-    apiKey: "AIzaSyB7HZbW8_Yi2yVyYD7Onc_dvvftSTJsyKc",
-    authDomain: "employee-data-99a21.firebaseapp.com",
-    databaseURL: "https://employee-data-99a21.firebaseio.com",
-    projectId: "employee-data-99a21",
-    storageBucket: "employee-data-99a21.appspot.com",
-    messagingSenderId: "256210453421"
-  };
-  firebase.initializeApp(config);
-}
+var config = {
+  apiKey: "AIzaSyB7HZbW8_Yi2yVyYD7Onc_dvvftSTJsyKc",
+  authDomain: "employee-data-99a21.firebaseapp.com",
+  databaseURL: "https://employee-data-99a21.firebaseio.com",
+  projectId: "employee-data-99a21",
+  storageBucket: "employee-data-99a21.appspot.com",
+  messagingSenderId: "256210453421"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
 
 $(document).ready(function() {
-  initFirebase();
+  var name = "";
+  var role = "";
+  var startDate = "";
+  var monthlyRate = "";
+});
 
-  $buttonSubmit.on("click", function() {
-    var name = $nameField.val();
-    var role = $roleField.val();
-    var startDate = $startDateField.val();
-    var monthlyRate = $monthlyRateField.val();
+$buttonSubmit.on("click", function(event) {
+  console.log("here");
+  event.preventDefault();
+  name = $nameField.val().trim();
+  role = $roleField.val().trim();
+  startDate = $startDateField.val().trim();
+  monthlyRate = $monthlyRateField.val().trim();
+
+  database.ref().push({
+    name: name,
+    role: role,
+    startDate: startDate,
+    monthlyRate: monthlyRate,
+    dateAdded: firebase.database.ServerValue.TIMESTAMP
   });
 });
